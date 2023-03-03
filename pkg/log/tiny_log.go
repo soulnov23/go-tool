246a9794dd3ea6a10ee27e2e13e62089291e1703
soulnov23 <22514389+soulnov23@users.noreply.github.com>
2023-03-03 15:28:13 +0800

    完善console打印颜色

--- pkg/log/tiny_log.go
+++ pkg/log/tiny_log.go
@@ -11,21 +11,28 @@ const (
 	ColorRed    = "\033[1;31m"
 	ColorGreen  = "\033[1;32m"
 	ColorYellow = "\033[1;33m"
-	ColorBlue   = "\033[1;34m"
-	ColorPink   = "\033[1;35m"
+	ColorWhite  = "\033[1;37m"
 	ColorReset  = "\033[m"
 
 	DefaultTimeFormat = "2006-01-02 15:04:05.000"
 )
 
-func Info(formatter string, args ...interface{}) {
-	fmt.Printf("%s %sINFO%s %s %s\n", time.Now().Format(DefaultTimeFormat), ColorYellow, ColorReset, utils.GetCaller(2), fmt.Sprintf(formatter, args...))
+func Debug(formatter string, args ...interface{}) {
+	fmt.Printf("%s%s DEBUG %s %s%s\n", ColorGreen, time.Now().Format(DefaultTimeFormat), utils.GetCaller(2), fmt.Sprintf(formatter, args...), ColorReset)
 }
 
-func Debug(formatter string, args ...interface{}) {
-	fmt.Printf("%s %sDEBUG%s %s %s\n", time.Now().Format(DefaultTimeFormat), ColorBlue, ColorReset, utils.GetCaller(2), fmt.Sprintf(formatter, args...))
+func Info(formatter string, args ...interface{}) {
+	fmt.Printf("%s%s INFO %s %s%s\n", ColorWhite, time.Now().Format(DefaultTimeFormat), utils.GetCaller(2), fmt.Sprintf(formatter, args...), ColorReset)
+}
+
+func Warn(formatter string, args ...interface{}) {
+	fmt.Printf("%s%s WARN %s %s%s\n", ColorYellow, time.Now().Format(DefaultTimeFormat), utils.GetCaller(2), fmt.Sprintf(formatter, args...), ColorReset)
 }
 
 func Error(formatter string, args ...interface{}) {
-	fmt.Printf("%s %sERROR%s %s %s\n", time.Now().Format(DefaultTimeFormat), ColorRed, ColorReset, utils.GetCaller(2), fmt.Sprintf(formatter, args...))
+	fmt.Printf("%s%s ERROR %s %s%s\n", ColorRed, time.Now().Format(DefaultTimeFormat), utils.GetCaller(2), fmt.Sprintf(formatter, args...), ColorReset)
+}
+
+func Fatal(formatter string, args ...interface{}) {
+	fmt.Printf("%s%s FATAL %s %s%s\n", ColorRed, time.Now().Format(DefaultTimeFormat), utils.GetCaller(2), fmt.Sprintf(formatter, args...), ColorReset)
 }
