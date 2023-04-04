78b77d4bbc6bff280a56368d0c64de861030c7e1
soulnov23 <22514389+soulnov23@users.noreply.github.com>
2023-04-04 21:03:42 +0800

    fix测试用例

--- pkg/copool/pool_test.go
+++ pkg/copool/pool_test.go
@@ -3,16 +3,10 @@ package copool
 import (
 	"runtime"
 	"testing"
-
-	"github.com/soulnov23/go-tool/pkg/log"
 )
 
 func TestCoPool(t *testing.T) {
-	zapLog, err := log.NewZapLog(log.DefaultConsoleLogConfig)
-	if err != nil {
-		t.Logf("log.NewZapLog: %s", err.Error())
-	}
-	pool := NewPool(zapLog.Errorf, 10)
+	pool := NewPool(t.Logf, 10)
 	handler := func() {
 		t.Logf("runtime.NumGoroutine: %d", runtime.NumGoroutine())
 	}
