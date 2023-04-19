97a6d04046acf1633702efa847926321fa7c8bc3
soulnov23 <22514389+soulnov23@users.noreply.github.com>
2023-04-19 22:12:35 +0800

    debug.Stack封装了runtime.Stack更合理

---
+++ pkg/coroutine/coroutine_test.go
@@ -0,0 +1,32 @@
+package coroutine
+
+import (
+	"runtime"
+	"runtime/debug"
+	"testing"
+
+	"github.com/soulnov23/go-tool/pkg/utils"
+)
+
+func TestStack(t *testing.T) {
+	defer func() {
+		if e := recover(); e != nil {
+			buffer := make([]byte, 10*1024)
+			runtime.Stack(buffer, false)
+			t.Logf("[PANIC] %v\n%s", e, utils.Byte2String(buffer))
+			t.Logf("[PANIC] %v\n%s", e, utils.Byte2String(debug.Stack()))
+		}
+	}()
+	panic("hello world")
+}
+
+func TestCoPool(t *testing.T) {
+	pool := New(t.Logf, 1)
+	for i := 0; i < 60; i++ {
+		fn := func(args ...any) {
+			t.Logf("key: %s, value: %d, runtime.NumGoroutine: %d", args[0], args[1], runtime.NumGoroutine())
+		}
+		pool.Run(fn, "index", i)
+	}
+	pool.Close()
+}
