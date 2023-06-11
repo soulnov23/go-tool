66a8ba95309caa4340615432f8ccd4b5ad804ab8
soulnov23 <22514389+soulnov23@users.noreply.github.com>
2023-06-11 21:48:53 +0800

    使用SIGINT来重启进程

--- build/bin/nohup.sh
+++ build/bin/nohup.sh
@@ -3,6 +3,9 @@
 ulimit -c unlimited
 export GOTRACEBACK=crash
 
-killall -9 go-tool
+pid=$(ps -ef | grep go-tool | grep -v grep | awk '{print $2}')
+if [ -n "${pid}" ]; then
+    kill -SIGINT ${pid}
+fi
 nohup ./go-tool -conf ../conf/go_tool.yaml >/dev/null 2>&1 &
-ps -ef | grep go-tool | grep -v grep | grep -v vi | grep -v tail | grep -v kill
+ps -ef | grep go-tool | grep -v grep
