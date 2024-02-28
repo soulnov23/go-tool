54745c778244e99c56ae2c5a37f7354a91599b2c
soulnov23 <22514389+soulnov23@users.noreply.github.com>
2024-02-28 11:30:19 +0800

    完善keepalive选项

--- pkg/netpoll/socket_option.go
+++ pkg/netpoll/socket_option.go
@@ -15,23 +15,34 @@ func SetSocketNonBlock(fd int) error {
 }
 
 func SetSocketReuseaddr(fd int) error {
-	var flags int = 1
-	return syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, flags)
+	return syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
 }
 
 func SetSocketReUsePort(fd int) error {
-	var flags int = 1
-	return syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, unix.SO_REUSEPORT, flags)
+	return syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, unix.SO_REUSEPORT, 1)
 }
 
 func SetSocketTCPNodelay(fd int) error {
-	var flags int = 1
-	return syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.TCP_NODELAY, flags)
+	return syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.TCP_NODELAY, 1)
 }
 
-func SetSocketKeepAlive(fd int) error {
-	var flags int = 1
-	return syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_KEEPALIVE, flags)
+// cnt保活探测次数
+// intvl保活探测间隔时间
+// idle连接空闲时间阈值
+func SetSocketKeepAlive(fd int, cnt int, intvl int, idle int) error {
+	if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_KEEPALIVE, 1); err != nil {
+		return err
+	}
+	if err := syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, syscall.TCP_KEEPCNT, cnt); err != nil {
+		return err
+	}
+	if err := syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, syscall.TCP_KEEPINTVL, intvl); err != nil {
+		return err
+	}
+	if err := syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, syscall.TCP_KEEPIDLE, idle); err != nil {
+		return err
+	}
+	return nil
 }
 
 func SetSocketCloseExec(fd int) {
