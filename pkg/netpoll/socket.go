77d54c22ef1d29a57f902cdbc32faee2ee2137c6
soulnov23 <22514389+soulnov23@users.noreply.github.com>
2024-03-20 15:51:56 +0800

    创建socket的时候直接设置non block和close exec

--- pkg/netpoll/socket.go
+++ pkg/netpoll/socket.go
@@ -9,55 +9,55 @@ import (
 func Socket(network string) (int, error) {
 	switch network {
 	case "tcp", "tcp4":
-		fd, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, unix.IPPROTO_TCP)
+		fd, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM|unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC, unix.IPPROTO_TCP)
 		if err != nil {
 			return 0, err
 		}
 		return fd, nil
 	case "tcp6":
-		fd, err := unix.Socket(unix.AF_INET6, unix.SOCK_STREAM, unix.IPPROTO_TCP)
+		fd, err := unix.Socket(unix.AF_INET6, unix.SOCK_STREAM|unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC, unix.IPPROTO_TCP)
 		if err != nil {
 			return 0, err
 		}
 		return fd, nil
 	case "udp", "udp4":
-		fd, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, unix.IPPROTO_UDP)
+		fd, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM|unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC, unix.IPPROTO_UDP)
 		if err != nil {
 			return 0, err
 		}
 		return fd, nil
 	case "udp6":
-		fd, err := unix.Socket(unix.AF_INET6, unix.SOCK_DGRAM, unix.IPPROTO_UDP)
+		fd, err := unix.Socket(unix.AF_INET6, unix.SOCK_DGRAM|unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC, unix.IPPROTO_UDP)
 		if err != nil {
 			return 0, err
 		}
 		return fd, nil
 	case "unix":
-		fd, err := unix.Socket(unix.AF_UNIX, unix.SOCK_STREAM, 0)
+		fd, err := unix.Socket(unix.AF_UNIX, unix.SOCK_STREAM|unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC, 0)
 		if err != nil {
 			return 0, err
 		}
 		return fd, nil
 	case "unixgram":
-		fd, err := unix.Socket(unix.AF_UNIX, unix.SOCK_DGRAM, 0)
+		fd, err := unix.Socket(unix.AF_UNIX, unix.SOCK_DGRAM|unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC, 0)
 		if err != nil {
 			return 0, err
 		}
 		return fd, nil
 	case "unixpacket":
-		fd, err := unix.Socket(unix.AF_UNIX, unix.SOCK_SEQPACKET, 0)
+		fd, err := unix.Socket(unix.AF_UNIX, unix.SOCK_SEQPACKET|unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC, 0)
 		if err != nil {
 			return 0, err
 		}
 		return fd, nil
 	case "ip", "ip4":
-		fd, err := unix.Socket(unix.AF_INET, unix.SOCK_RAW, unix.IPPROTO_IP)
+		fd, err := unix.Socket(unix.AF_INET, unix.SOCK_RAW|unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC, unix.IPPROTO_IP)
 		if err != nil {
 			return 0, err
 		}
 		return fd, nil
 	case "ip6":
-		fd, err := unix.Socket(unix.AF_INET6, unix.SOCK_RAW, unix.IPPROTO_IPV6)
+		fd, err := unix.Socket(unix.AF_INET6, unix.SOCK_RAW|unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC, unix.IPPROTO_IPV6)
 		if err != nil {
 			return 0, err
 		}
