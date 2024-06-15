fc9b8baa61e949efa2ad23f8a78764b4f4de4477
soulnov23 <22514389+soulnov23@users.noreply.github.com>
2024-06-16 02:01:45 +0800

    使用gorm

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -3,15 +3,13 @@ package mysql
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
-	"github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-type DB struct {
-	*sql.DB
-}
-
-func New(ctx context.Context, dsn string, opts ...Option) (*DB, error) {
+func New(ctx context.Context, dsn string, opts ...Option) (*gorm.DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, err
@@ -33,77 +31,11 @@ func New(ctx context.Context, dsn string, opts ...Option) (*DB, error) {
 	db.SetMaxOpenConns(defaultOpts.MaxOpenConns)
 	db.SetConnMaxLifetime(defaultOpts.ConnMaxLifetime)
 	db.SetConnMaxIdleTime(defaultOpts.ConnMaxIdleTime)
-	return &DB{DB: db}, nil
-}
-
-func (impl *DB) QueryResult(ctx context.Context, sql string) ([]map[string]any, error) {
-	var result []map[string]any
-	rows, err := impl.DB.QueryContext(ctx, sql)
+	orm, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: db,
+	}))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("gorm.Open: %v", err)
 	}
-	defer rows.Close()
-	columns, err := rows.Columns()
-	if err != nil {
-		return nil, err
-	}
-	columnLen := len(columns)
-	var dest []any
-	// 不能用make初始化，要赋值指针
-	for i := 0; i < columnLen; i++ {
-		var destInterface any
-		dest = append(dest, &destInterface)
-	}
-	for rows.Next() {
-		if err := rows.Scan(dest...); err != nil {
-			return nil, err
-		}
-		temp := make(map[string]any)
-		for index, column := range columns {
-			temp[column] = dest[index]
-		}
-		result = append(result, temp)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func DuplicateEntry(err error) bool {
-	mysqlErr, ok := err.(*mysql.MySQLError)
-	if ok && mysqlErr.Number == 1062 /*ER_DUP_ENTRY*/ {
-		return true
-	}
-	return false
-}
-
-func Escape(value string) string {
-	var dest []rune
-	var escape rune
-	for _, character := range value {
-		escape = 0
-		switch character {
-		case 0: /* Must be escaped for 'mysql' */
-			escape = '0'
-		case '\n': /* Must be escaped for logs */
-			escape = 'n'
-		case '\r':
-			escape = 'r'
-		case '\\':
-			escape = '\\'
-		case '\'':
-			escape = '\''
-		case '"': /* Better safe than sorry */
-			escape = '"'
-		case '\032': /* This gives problems on Win32 */
-			escape = 'Z'
-		}
-		if escape != 0 {
-			dest = append(dest, '\\', escape)
-		} else {
-			dest = append(dest, character)
-		}
-	}
-	return string(dest)
+	return orm, nil
 }
