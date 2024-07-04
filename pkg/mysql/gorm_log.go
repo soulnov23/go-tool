435d370846847d04fe226fee7092067a39cc452e
soulnov23 <22514389+soulnov23@users.noreply.github.com>
2024-07-04 11:26:13 +0800

    判断ErrRecordNotFound

--- pkg/mysql/gorm_log.go
+++ pkg/mysql/gorm_log.go
@@ -2,10 +2,12 @@ package mysql
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/soulnov23/go-tool/pkg/log"
+	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
@@ -62,34 +64,34 @@ func (l *gormLogger) Error(ctx context.Context, msg string, data ...interface{})
 func (l *gormLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
 	elapsed := time.Since(begin)
 	sql, rows := fc()
-	if err != nil {
+	if err != nil && (!errors.Is(err, gorm.ErrRecordNotFound) || !l.IgnoreRecordNotFoundError) {
 		if rows == -1 {
-			l.Logger.Errorf(errorFormatter, err, float64(elapsed.Nanoseconds())/1e6, "-", sql)
+			l.Errorf(errorFormatter, err, float64(elapsed.Nanoseconds())/1e6, "-", sql)
 		} else {
-			l.Logger.Errorf(errorFormatter, err, float64(elapsed.Nanoseconds())/1e6, rows, sql)
+			l.Errorf(errorFormatter, err, float64(elapsed.Nanoseconds())/1e6, rows, sql)
 		}
 		return
 	}
 
 	if elapsed > l.SlowThreshold && l.SlowThreshold != 0 {
 		if rows == -1 {
-			l.Logger.Warnf(warnFormatter, fmt.Sprintf("SLOW SQL >= %v", l.SlowThreshold), float64(elapsed.Nanoseconds())/1e6, "-", sql)
+			l.Warnf(warnFormatter, fmt.Sprintf("SLOW SQL >= %v", l.SlowThreshold), float64(elapsed.Nanoseconds())/1e6, "-", sql)
 		} else {
-			l.Logger.Warnf(warnFormatter, fmt.Sprintf("SLOW SQL >= %v", l.SlowThreshold), float64(elapsed.Nanoseconds())/1e6, rows, sql)
+			l.Warnf(warnFormatter, fmt.Sprintf("SLOW SQL >= %v", l.SlowThreshold), float64(elapsed.Nanoseconds())/1e6, rows, sql)
 		}
 		return
 	}
 
 	if rows == -1 {
-		l.Logger.Infof(infoFormatter, float64(elapsed.Nanoseconds())/1e6, "-", sql)
+		l.Infof(infoFormatter, float64(elapsed.Nanoseconds())/1e6, "-", sql)
 	} else {
-		l.Logger.Infof(infoFormatter, float64(elapsed.Nanoseconds())/1e6, rows, sql)
+		l.Infof(infoFormatter, float64(elapsed.Nanoseconds())/1e6, rows, sql)
 	}
 }
 
 // ParamsFilter filter params
 func (l *gormLogger) ParamsFilter(ctx context.Context, sql string, params ...interface{}) (string, []interface{}) {
-	if l.Options.ParameterizedQueries {
+	if l.ParameterizedQueries {
 		return sql, nil
 	}
 	return sql, params
