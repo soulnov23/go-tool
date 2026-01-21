2115414374d747aa69904fbc5a0ae0fb402952ad
soulnov23 <22514389+soulnov23@users.noreply.github.com>
2026-01-21 15:22:55 +0800

    chmod增加判断

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -15,16 +15,18 @@ main() {
         cp -rf /data/home/project/go-tool/.vscode/tasks.json ./.vscode
     fi
     cp -rf /data/home/project/go-tool/.vscode/settings.json ./.vscode
-    chmod -R 644 ./
-    find . -type d -name build | while read -r dir; do
-        bin_dir="${dir}/bin"
-        if [ -d "$bin_dir" ]; then
-            find "$bin_dir" -maxdepth 1 -type f -exec chmod +x {} \;
-        fi
-    done
-    find . -type f -name "*.sh" | while read -r file; do
-        chmod +x "${file}"
-    done
+    if [ "$1" == "chmod" ]; then
+        chmod -R 644 ./
+        find . -type d -name build | while read -r dir; do
+            bin_dir="${dir}/bin"
+            if [ -d "$bin_dir" ]; then
+                find "$bin_dir" -maxdepth 1 -type f -exec chmod +x {} \;
+            fi
+        done
+        find . -type f -name "*.sh" | while read -r file; do
+            chmod +x "${file}"
+        done
+    fi
 }
 
 main "$@"
