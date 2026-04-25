9279e7bf62c6fdeb328585dbfb23680566da827d
soulnov23 <22514389+soulnov23@users.noreply.github.com>
2026-04-25 13:53:27 +0800

    update

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -16,6 +16,9 @@ main() {
     fi
     cp -rf /data/home/project/go-tool/.vscode/settings.json ./.vscode
     cp -rf /data/home/project/go-tool/.claude ./
+    goinit ./
+    shellfmt ./
+    chmodinit ./
 }
 
 main "$@"
