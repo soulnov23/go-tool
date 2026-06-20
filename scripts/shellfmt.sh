27a63b6f86f252c15970a089bddf27b9bcbde423
soulnov23 <22514389+soulnov23@users.noreply.github.com>
2026-06-20 18:16:04 +0800

    update

--- scripts/shellfmt.sh
+++ scripts/shellfmt.sh
@@ -3,12 +3,8 @@
 set -x
 set -e
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <path>"
-    exit 1
-fi
 # --language-dialect
 # --indent
 # --case-indent switch cases will be indented
 # --write write result to file instead of stdout
-shfmt -ln bash -i 4 -ci -w $1
+shfmt -ln bash -i 4 -ci -w .
